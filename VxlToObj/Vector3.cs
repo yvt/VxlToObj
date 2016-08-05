0da5e1b78d461fdedd59ba9ebb56a1a97619cdd7
yvt <i@yvt.jp>
2016-08-05 15:20:18 +0900

    Add the missing Vector3.cs

---
+++ VxlToObj/Vector3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace VxlToObj
+{
+	public struct Vector3
+	{
+		public float X { get; set; }
+		public float Y { get; set; }
+		public float Z { get; set; }
+
+		public Vector3(float x, float y, float z)
+		{
+			X = x;
+			Y = y;
+			Z = z;
+		}
+	}
+}
+
