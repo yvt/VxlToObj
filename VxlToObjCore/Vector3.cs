b5c15b4df85531b98330519801743e779bde2873
yvt <i@yvt.jp>
2016-08-05 15:44:58 +0900

    Make API more consistent

--- VxlToObjCore/Vector3.cs
+++ VxlToObjCore/Vector3.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+
 namespace VxlToObj.Core
 {
 	public struct Vector3
@@ -12,6 +14,32 @@ namespace VxlToObj.Core
 			X = x;
 			Y = y;
 			Z = z;
+		}
+
+		public float this[Axis3 axis]
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			get
+			{
+				switch (axis)
+				{
+					case Axis3.X: return X;
+					case Axis3.Y: return Y;
+					case Axis3.Z: return Z;
+					default: throw new ArgumentOutOfRangeException(nameof(axis));
+				}
+			}
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			set
+			{
+				switch (axis)
+				{
+					case Axis3.X: X = value; break;
+					case Axis3.Y: Y = value; break;
+					case Axis3.Z: Z = value; break;
+					default: throw new ArgumentOutOfRangeException(nameof(axis));
+				}
+			}
 		}
 	}
 }
